d70971fa685d8a48ea1b4e1808b7b533e77646c5
vinivid <frato.vini@gmail.com>
2025-06-05 15:55:37 -0300

    init .py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import requests as req
 from src.CursosUsp import CursosUsp
 
 def main():
-    BeautifulSoup(req.get('https://uspdigital.usp.br/jupiterweb/jupCarreira.jsp?codmnu=8275').text).find()
 
 if __name__ == "__main__":
     main()
--- src/__init__.py
+++
@@ -1,3 +0,0 @@
-class CursosUsp:
-    def __init__(self) -> None:
-        print('ok')
