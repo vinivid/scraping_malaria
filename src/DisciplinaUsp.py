5f421eb9c2af0c2faf7f0fbfa653763b036c45a0
almeidarodrigo04 <rodrigosilvalmeida10@gmail.com>
2025-06-12 12:59:53 -0300

    bbb

--- src/DisciplinaUsp.py
+++ src/DisciplinaUsp.py
@@ -4,12 +4,10 @@ class DisciplinaUsp:
     nome : str
     cred_aula : int
     cred_trab : int
-    carg_h : int
-    carg_h_est : int
-    carg_h_pcc : int
-    att_tpa: str
-
-    #sta_no_curso : dict[str, str] aparentemente essa informação n eh relevante para a diciplina, segundo a malaria
+    CH : int
+    CE : int
+    CP : int
+    ATPA : int
 
     #Perguntar dps pro jiboia! n entendi como eu vou receber os parametros, como vai ser a chamada  da func na """"main""""
     def __init__(self):
@@ -58,7 +56,7 @@ class DisciplinaUsp:
         :return: Carga Horária da disciplina.
         :rtype: int
         """
-        return self.carg_h
+        return self.CH
 
     def get_carga_horaria_estagio(self) -> int:
         """
@@ -67,29 +65,33 @@ class DisciplinaUsp:
         :return: Carga Horária de Estágio da disciplina.
         :rtype: int
         """
-        return self.carg_h_est
+        return self.CE
 
-    def get_carga_horaria_praticas_componentes_curriculares(self):
+    def get_carga_horaria_praticas_componentes_curriculares(self) -> int:
         """
-        Pega o inteiro de carga horária de práticas como componentes curriculares disciplina.
+        Pega o inteiro de carga horária de práticas como componentes curriculares da disciplina.
 
         :return: Carga Horária de Práticas Como Componentes Curriculares da disciplina.
         :rtype: int
         """
-        return self.carg_h_pcc
+        return self.CP
     
-    #ainda n entendi oq eh isso na real
-    def get_atividades_teorico_praticas_aprofundamento(self):
-        return self.att_tpa
+    def get_atividades_teorico_praticas_aprofundamento(self) -> int:
+        """
+        Pega o inteiro de ativides teórico práticas de aprofundamento da disciplina.
+
+        :return: Atividades Teórico-Práticas de Aprofundamento da disciplina.
+        :rtype: int
+        """
+        return self.ATPA
 
     def __str__(self) -> str:
         disciplina_str = f'\nCódigo: {self.codigo}'
         disciplina_str += f'\nNome: {self.nome}'
         disciplina_str += f'\nCréditos Aula: {self.cred_aula}'
         disciplina_str += f'\nCréditos Trabalho: {self.cred_trab}'
-        disciplina_str += f'\nCarga Horária: {self.carg_h}'
-        disciplina_str += f'\nCarga Horária Estágio: {self.carg_h_est}'
-        disciplina_str += f'\nCarga Horária PCC: {self.carg_h_pcc}'
-        #eu preciso entender esse campo
-        disciplina_str += f'\n{self.att_tpa}\n'
+        disciplina_str += f'\nCarga Horária: {self.CH}'
+        disciplina_str += f'\nCarga Horária Estágio: {self.CE}'
+        disciplina_str += f'\nCarga Horária PCC: {self.CP}'
+        disciplina_str += f'\nAtividades TPA: {self.ATPA}\n'
         return disciplina_str
